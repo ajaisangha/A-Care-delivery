8d2b33a83555ad07ad1fe68bcb4a129a612d1cee
ajaisangha <ajaisangha@gmail.com>
2025-11-04 01:32:09 +0000

    bug fix 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Carousel } from "react-bootstrap";
-import "./Home.css"; // Use separate CSS file
 
 export default function Home() {
   return (
